7492480be6ae6eada8f64e6356379205afac0b05
Aditxgupta <99131237+Aditxgupta@users.noreply.github.com>
2025-03-29 01:15:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -316,4 +316,5 @@ def download_markdown(file_id):
 
 # --- Main Execution ---
 if __name__ == '__main__':
-    app.run(debug=True) # Turn off debug in production
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
